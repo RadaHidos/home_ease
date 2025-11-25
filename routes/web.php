fe19a0e54654bfa7905ec5301f52679d3c8bd9d3
RadaHidos <120414438+RadaHidos@users.noreply.github.com>
2025-11-25 21:14:26 +0100

    Switch to CRUD - Resourceful routes for categories

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,7 @@ Route::get('services', [\App\Http\Controllers\ServiceController::class, 'index']
 Route::get('services/{id}', [\App\Http\Controllers\ServiceController::class, 'show'])->name('services.show');
 
 
-Route::get('categories', [\App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index');
-Route::get('categories/create', [\App\Http\Controllers\CategoryController::class, 'create'])->name('categories.create');
-Route::post('categories', [\App\Http\Controllers\CategoryController::class, 'store'])->name('categories.store');
-Route::get('categories/{id}/edit', [\App\Http\Controllers\CategoryController::class, 'edit'])->name('categories.edit');
-Route::put('categories/{id}', [\App\Http\Controllers\CategoryController::class, 'update'])->name('categories.update');
-Route::delete('categories/{id}', [\App\Http\Controllers\CategoryController::class, 'destroy'])->name('categories.destroy');
-
-Route::get('categories/{id}', [\App\Http\Controllers\CategoryController::class, 'show'])->name('categories.show');
+Route::resource('categories', \App\Http\Controllers\CategoryController::class);
 
 
 Route::get('/dashboard', function () {

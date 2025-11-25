0bdf92a5b4ca2219941906db5a00f9281faf097f
RadaHidos <120414438+RadaHidos@users.noreply.github.com>
2025-11-25 18:50:00 +0100

    Created edit view

---
+++ resources/views/categories/edit.blade.php
@@ -0,0 +1,30 @@
+<x-site-layout title='Edit {{$category->name}}'>
+
+    <form action="/categories/{{$category->id}}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="flex flex-col gap-1">
+            <label class="text-sm text-slate-600">Category name</label>
+            <input
+                type="text"
+                placeholder="Add a new category"
+                name="name"
+                value="{{$category->name}}"
+                class="rounded-lg border border-slate-200 px-3 py-2 text-sm 
+                   focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500"
+                required>
+            </input>
+        </div>
+
+        <button
+            type="submit"
+            class="w-full mt-4 bg-blue-500 text-white py-2.5 rounded-lg text-sm font-medium 
+               hover:bg-blue-600 transition cursor-pointer">
+            Update
+        </button>
+    </form>
+
+
+
+</x-site-layout>

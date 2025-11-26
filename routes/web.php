8b714858c72f562fb2efe1d875b302920fab473a
RadaHidos <120414438+RadaHidos@users.noreply.github.com>
2025-11-26 22:27:12 +0100

    Created a middleware for IsAdmin
    to make the categories not available for editing or deleting for user, and only for admin

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,17 @@ Route::get('services/{id}', [\App\Http\Controllers\ServiceController::class, 'sh
 Route::resource('categories', \App\Http\Controllers\CategoryController::class)->only(['index', 'show']);
 
 
-Route::resource('admin/categories', \App\Http\Controllers\AdminCategoryController::class);
-Route::resource('admin/services', \App\Http\Controllers\AdminServiceController::class)->middleware('auth');
+
 
 Route::get('/dashboard', function () {
     return view('userzone.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('admin/categories', \App\Http\Controllers\AdminCategoryController::class)
+    ->middleware('is_admin');
+    Route::resource('admin/services', \App\Http\Controllers\AdminServiceController::class);
+
     Route::get('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'destroy'])->name('profile.destroy');

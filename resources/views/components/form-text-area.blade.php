aae73eff2fdac95dc196291c43816024b2222783
RadaHidos <120414438+RadaHidos@users.noreply.github.com>
2025-11-26 01:34:14 +0100

    Component for textarea for service content

---
+++ resources/views/components/form-text-area.blade.php
@@ -0,0 +1,15 @@
+<div class="flex flex-col gap-1">
+    <label class="text-sm text-slate-600">{{$label}}</label>
+    <textarea
+
+        placeholder="{{$placeholder}}"
+        rows="{{$rows}}"
+        name="{{$name}}"
+        class="rounded-lg border border-slate-200 px-3 py-2 text-sm 
+                   focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500">{{old($name, $value)}}
+
+    </textarea>
+</div>
+@error($name)
+<div class='text-red-500'>{{$message}}</div>
+@enderror

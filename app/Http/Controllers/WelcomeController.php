702f7b76666783ad2033663253f653489ddeb7e6
RadaHidos <120414438+RadaHidos@users.noreply.github.com>
2025-11-27 17:19:03 +0100

    DESIGN - Designed the Welcome page

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Service;
+use App\Models\Category;
 
 class WelcomeController extends Controller
 {
     public function __invoke()
     {
+        $services   = Service::take(3)->get();
+        $categories = Category::all();
+
+        return view('welcome', compact('services', 'categories'));
         return view('welcome');
     }
     //

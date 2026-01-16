38a49ae43809fa653ed67f56ec170607d55c0161
RadaHidos <120414438+RadaHidos@users.noreply.github.com>
2026-01-16 11:47:18 +0100

    form-text to formtext

--- resources/views/admin/services/create.blade.php
+++ resources/views/admin/services/create.blade.php
@@ -6,7 +6,7 @@
 
 
         <div class="max-w-xl mx-auto bg-white p-6 rounded-xl shadow-md space-y-4">
-            <x-form-text name="title" label="Add service name" placeholder="What service do you provide?" />
+            <x-formtext name="title" label="Add service name" placeholder="What service do you provide?" />
 
             <x-form-textarea name="content" label="Add service description" placeholder="Describe your service and give important details" rows="10" />
             
